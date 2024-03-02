3d51db01a24b60fce78bb29b1a5e6088879e4029
Ashwinnbr007 <ashwinnambiarbusiness@gmail.com>
2024-03-02 11:02:22 +0530

    1st challenge

--- 1. buggy-product/Challenge.js
+++ 1. buggy-product/Challenge.js
@@ -3,7 +3,10 @@
 // (Ex - For input [0, 0, 1, 1, 1], the bug was introduced in version 2 and the function should return 1)
 
 function lastBugFreeVersion(versions) {
-  // Your implementation here
+  for (let i = 0; i < versions.length; i++) {
+    if (versions[i] == 1)
+      return 1
+  }
+  return 0
 }
-
-lastBugFreeVersion([0, 0, 1, 1, 1]);
+console.log(lastBugFreeVersion([0, 0, 1, 1, 1]));
